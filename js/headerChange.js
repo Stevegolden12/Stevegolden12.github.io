f5d647885c735fb537a2d37f9302dbf7654dc7f9
Steve Golden <sgolden2525@outlook.com>
2018-11-16 13:12:59 -0600

    Change bottom color and added comment
    Changed bottom border nav colors when scrolling.
    
    Added comment to direct where bottom color is coming from

--- js/headerChange.js
+++ js/headerChange.js
@@ -4,11 +4,14 @@
 
 const HEADER = $('HEADER');
 
+var c = document.createComment("Style header border change is in JS");
+HEADER[0].appendChild(c);
+
 addEventListener('scroll', function () {
   if (HEADER.offset().top > HEADER[0].clientHeight) {
-    HEADER[0].style.borderBottom = "solid 1px hsl(0, 0%, 90%)";
+    HEADER[0].style.borderBottom = "solid 1px hsl(0, 0%, 100%)";
   } else {
-    HEADER[0].style.borderBottom = "solid 1px hsl(208, 98%, 26%)";
+    HEADER[0].style.borderBottom = "solid 1px hsl(356, 39.8%, 34.5%)";    
   }
 });
 
